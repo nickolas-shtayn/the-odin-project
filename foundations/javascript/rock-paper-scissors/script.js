583de6fd5519e79c49498c6581f98ae3953ddb8d
Nickolas Shtayn <nickolasshtayn@protonmail.com>
2024-08-27 12:29:24 -0500

    Add project code structure

--- foundations/javascript/rock-paper-scissors/script.js
+++ foundations/javascript/rock-paper-scissors/script.js
@@ -1,1 +1,19 @@
-console.log(1)
+function getComputerChoice(){
+    // randomly return rock, paper or scissors
+}
+
+function getHumanChoice(){
+    // return one of the valid choices depending on the user input
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+function playRound(humanChoice, computerChoice){
+    // return a string value representing the round winner
+    // incremenet humanScore or computerScore variables based on winner
+}
+
