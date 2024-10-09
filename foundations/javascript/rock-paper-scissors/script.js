ddfb1b74720c084ce00360b263169e85d59d4be4
Nickolas Shtayn <nickolasshtayn@protonmail.com>
2024-10-09 14:21:40 -0500

    Complete logic for rps-ui

--- foundations/javascript/rock-paper-scissors/script.js
+++ foundations/javascript/rock-paper-scissors/script.js
@@ -1,42 +1,68 @@
 const TOTAL_ROUNDS = 5;
 const CHOICES = ["rock", "paper", "scissors"]
 
+// function that gets a random choice for the computer
 function getComputerChoice(){
     let randomIndex = Math.floor(Math.random() * CHOICES.length);
     return CHOICES[randomIndex];
 }
 
-function getHumanChoice(){
-    let userAnswer = prompt("What is your choice?");
+const userChoices = document.querySelector("#choices");
 
-    // normalize format and match choices format
-    userAnswer = userAnswer.toLowerCase();
+let score = { human: 0, computer: 0};
+let scoreDisplay = document.querySelector("#running-score");
 
-    if (CHOICES.includes(userAnswer)) {
-        return userAnswer;
-    } else {
-        console.log('Your choices are "Rock", "Paper", or "Scissors"')
-        return getHumanChoice();
-        }
+userChoices.addEventListener("click", getHumanChoice);
+
+function getHumanChoice(event) {
+    let target = event.target;
+    let userChoice;
+
+    switch(target.id) {
+        case 'rock':
+            userChoice = "rock";
+            break;
+        case 'paper':
+            userChoice = "paper";
+            break;
+        case 'scissors':
+            userChoice =  "scissors";
+            break;
     }
+    playRound(userChoice, getComputerChoice(), score);
+
+    if (score.human == 5) {
+        scoreDisplay.innerText = "Human won!"
+        score = { human: 0, computer: 0};
+    } else if (score.computer == 5) {
+        scoreDisplay.innerText = "Computer won!"
+        score = { human: 0, computer: 0};
+    } else {
+        scoreDisplay.innerText = `Human: ${score.human}, Computer ${score.computer}`;
+    }
+}
+
 
 function playRound(humanChoice, computerChoice, score){
+
     const outcomes = {
         "rockrock": "It's a tie! Both chose rock",
         "rockpaper": "You lose! Paper beats rock",
         "rockscissors": "You win! Rock beats scissors",
         "paperrock": "You win! Paper beats rock",
-        "paperpapper": "It's a tie! Both chose paper",
+        "paperpaper": "It's a tie! Both chose paper",
         "paperscissors": "You lose! Scissors beat paper",
         "scissorsrock": "You lose! Rock beats scissors",
-        "scissorspaper": "You win! Scissors beats raper",
+        "scissorspaper": "You win! Scissors beats paper",
         "scissorsscissors": "It's a tie! Both chose scissors" 
     };
 
     const result = outcomes[`${humanChoice}${computerChoice}`];
 
+    let resultDisplay = document.querySelector("#result");
+
     if(result){
-        console.log(result);
+        resultDisplay.innerText = result;
 
         if (result.includes("win")){
             score.human += 1;
@@ -46,25 +72,4 @@ function playRound(humanChoice, computerChoice, score){
     } else {
         console.error("Unexpected error: Invalid game state.")
     }
-    return score;
 }
-
-function playGame(){
-    let score = { human: 0, computer: 0};
-
-    for (let i = 1; i <= TOTAL_ROUNDS; i++){
-        const humanChoice = getHumanChoice(); 
-        const computerChoice = getComputerChoice();
-        score = playRound(humanChoice, computerChoice, score);
-    }
-
-    if (score.human > score.computer){
-        return console.log("You're the winner!")
-    } else if (score.human < score.computer){
-        return console.log("Computer is the winner")
-    } else {
-        return console.log("It's a tie!")
-    }
-}
-
-playGame();
